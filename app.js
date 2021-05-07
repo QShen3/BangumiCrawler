89ea76b909eda96d123bfc2f1eacc783bba144f4
Shen <qazxdrcssc2006@163.com>
2021-05-07 22:56:07 +0800

    Make it simple

--- app.js
+++ app.js
@@ -1,257 +1,71 @@
-const fs = require('fs');
-const urllib = require('urllib');
+import mongodb from "mongodb";
+import urllib from "urllib";
+import range from "range"
+import config from "./config.js";
 
-const { Bangumi, Crt, Actor, Staff, Episode, BroadCaster, Network } = require('./models/index');
-const config = require('./config');
-
-const failedList = [];
-var count = 0;
-
-function resolveDate(input) {
-    if (input.indexOf('-') >= 0) {
-        return input;
-    }
-    else if (input.indexOf('.') >= 0) {
-        return input.split('.').reverse().join('-');
-    }
-    else if (input.indexOf('年') >= 0) {
-        return input.replace('年', '-').replace('月', '-').replace('日', '');
-    }
-}
-
-function resolveDuration(input) {
-    if (input.indexOf(':') >= 0) {
-        let duration = input.split(':');
-        if (duration.length == 3) {
-            return (parseInt(duration[0]) || 0) * 3600 + (parseInt(duration[1]) || 0) * 60 + (parseInt(duration[2]) || 0);
-        }
-        else if (duration.length == 2) {
-            return parseInt(duration[0]) * 60 + parseInt(duration[1]);
-        }
-        else {
-            return 0;
-        }
-    }
-    else if (input.indexOf('m') >= 0) {
-        let hour = 0;
-        if (input.indexOf('h') >= 0) {
-            input = input.split('h');
-            hour = parseInt(input[0]);
-            input = input[1];
-        }
-        let minute = parseInt(input.replace('m', ''));
-        return hour * 3600 + minute * 60;
+const uri = (() => {
+    if (config.mongoDB.user != '' && config.mongoDB.password != '') {
+        return `mongodb://${config.mongoDB.user}:${config.mongoDB.password}@${config.mongoDB.host}:${config.mongoDB.port}`;
     }
     else {
-        return 0;
+        return `mongodb://${config.mongoDB.host}:${config.mongoDB.port}`;
     }
-}
-
-async function getDoc(bangumiId) {
-    try {
-        var { data, res } = await urllib.request(`http://api.bgm.tv/subject/${bangumiId}?responseGroup=large`, { gzip: true });
-    }
-    catch (err) {
-        console.error(`${bangumiId}: ${err.message}`);
-        failedList.push(bangumiId);
-        return;
-    }
-    if (res.statusCode != 200) {
-        if ([301, 302, 303, 304].indexOf(res.statusCode) >= 0) {
-            console.log(`${bangumiId}: 3XX`);
-            return;
-        }
-        console.error(`${bangumiId}: ${res.statusCode}`);
-        failedList.push(bangumiId);
-        return;
-    }
-
-    try {
-        var bgmObject = JSON.parse(data);
-    }
-    catch (err) {
-        console.error(`${bangumiId}: ${err.message}`);
-        failedList.push(bangumiId);
-        return;
-    }
-
-    if (bgmObject.type !== 2) {
-        console.error(`${bangumiId}: not bangumi`);
-        return;
-    }
-
-    console.log(`${bgmObject.id}: ${bgmObject.name} | ${bgmObject.name_cn || ''}`);
-
-    let bangumi = new Bangumi({
-        name: bgmObject.name,
-        name_cn: bgmObject.name_cn,
-        summary: bgmObject.summary,
-        images: bgmObject.images,
-    });
-    bangumi.air_date = resolveDate(bgmObject.air_date);
-    bangumi.other_website.push({
-        name: 'Bangumi',
-        url: `http://bangumi.tv/subject/${bangumiId}`,
-        id: bangumiId
-    });
-
-    for (let staffObject of (bgmObject.staff || [])) {
-        let staff = await Staff.findOne({ name: staffObject.name }).exec();
-        if (!staff) {
-            staff = new Staff({
-                name: staffObject.name,
-                name_cn: staffObject.name_cn,
-                images: staffObject.images,
-            });
-            staff.info = {};
-            if (staffObject.info && staffObject.info.gender) {
-                staff.info.gender = staffObject.info.gender;
-            }
-            staff.jobs = staff.jobs.concat(staffObject.jobs);
-        }
-        else {
-            staff.jobs = Array.from(new Set(staff.jobs.concat(staffObject.jobs)));
-        }
-        try {
-            staff = await staff.save();
-        }
-        catch (err) {
-            console.error(`${bangumiId}: ${err.message}`);
-            failedList.push(bangumiId);
-            return;
-        }
-        bangumi.staff.push({
-            name: staff.name,
-            name_cn: staff.name_cn,
-            jobs: staffObject.jobs,
-            id: staff._id
-        });
-    }
-
-    for (let crtObject of (bgmObject.crt || [])) {
-        let crt = await Crt.findOne({ name: crtObject.name }).exec();
-        if (!crt) {
-            crt = new Crt({
-                name: crtObject.name,
-                name_cn: crtObject.name_cn,
-                images: crtObject.images,
-            });
-            crt.info = {};
-            if (crtObject.info && crtObject.info.gender) {
-                crt.info.gender = crtObject.info.gender;
-            }
-        }
-        for (let actorObject of (crtObject.actors || [])) {
-            let actor = await Actor.findOne({ name: actorObject.name }).exec();
-            if (!actor) {
-                actor = new Actor({
-                    name: actorObject.name,
-                    name_cn: actorObject.name_cn,
-                    images: actorObject.images
-                });
-                try {
-                    actor = await actor.save();
-                }
-                catch (err) {
-                    console.error(`${bangumiId}: ${err.message}`);
-                    failedList.push(bangumiId);
-                    return;
-                }
-            }  
-            let flag = true;
-            for (let cv of crt.cv) {
-                if (cv.id.toString() == actor._id.toString()) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                crt.cv.push({
-                    name: actor.name,
-                    name_cn: actor.name_cn,
-                    id: actor._id
-                });
-            }
-        }
-        crt.cv = Array.from(new Set(crt.cv));
-        try {
-            crt = await crt.save();
-        }
-        catch (err) {
-            console.error(`${bangumiId}: ${err.message}`);
-            failedList.push(bangumiId);
-            return;
-        }
-        bangumi.crt.push(crt._id);
-    }
-
-    for (let epObject of (bgmObject.eps || [])) {
-        let episode = new Episode({
-            name: epObject.name,
-            name_cn: epObject.name_cn,
-            duration: resolveDuration(epObject.duration),
-            air_date: resolveDate(epObject.airdate),
-            desc: epObject.desc,
-            status: epObject.status === 'Air' ? 1 : 0
-        });
-        try {
-            episode = await episode.save();
-        }
-        catch (err) {
-            console.error(`${bangumiId}: ${err.message}`);
-            failedList.push(bangumiId);
-            return;
-        }
-        if (epObject.type == 0) {
-            bangumi.ep.push({
-                title: `第${epObject.sort}话`,
-                name: episode.name,
-                name_cn: episode.name_cn,
-                status: episode.status,
-                id: episode._id
-            });
-        }
-        else if (epObject.type == 1) {
-            bangumi.sp.push({
-                title: `第${epObject.sort}话`,
-                name: episode.name,
-                name_cn: episode.name_cn,
-                status: episode.status,
-                id: episode._id
-            });
-        }
-    }
-
-    try {
-        bangumi = await bangumi.save();
-    }
-    catch (err) {
-        console.error(`${bangumiId}: ${err.message}`);
-        failedList.push(bangumiId);
-        return;
-    }
-    console.log(`${bgmObject.name} | ${bgmObject.name_cn || ''} has been inserted.`);
-
-    return;
-}
-
-(async () => {
-    let pos = process.argv.indexOf('-f');
-    if (pos >= 0) {
-        let file = process.argv[pos + 1];
-        let list = fs.readFileSync(file).toString();
-        list = list.split(',');
-        for (let i of list) {
-            await getDoc(i);
-        }
-        fs.writeFileSync('./failedList.txt', failedList.toString());
-    }
-    else {
-        for (let i = config.start; i <= config.end; i++) {
-            await getDoc(i);
-        }
-        fs.writeFileSync('./failedList.txt', failedList.toString());
-    }
-
-    process.exit(0);
 })();
+
+const client = new mongodb.MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function main() {
+    await client.connect();
+
+    const database = client.db(config.mongoDB.db);
+    const anime = database.collection("anime");
+    const game = database.collection("game");
+
+    let bangumiList = range.range(config.start, config.end + 1);
+
+    for (let i of bangumiList) {
+        try {
+            var {data, res} = await urllib.request(`http://api.bgm.tv/subject/${i}?responseGroup=large`, { gzip: true });
+        } catch (err) {
+            console.error(`${i}: ${err.message}`);
+            bangumiList.push(i);
+            continue;
+        }
+        if (res.statusCode != 200) {
+            if ([301, 302, 303, 304].indexOf(res.statusCode) >= 0) {
+                console.log(`${i}: redirect`);
+                continue;
+            }
+            console.error(`${i}: ${res.statusCode}`);
+            bangumiList.push(i);
+            continue;
+        }
+
+        try {
+            var bangumiObject = JSON.parse(data);
+            if (bangumiObject.type == 2) {
+                await anime.insertOne(bangumiObject);
+            } else if (bangumiObject.type == 4) {
+                await game.insertOne(bangumiObject);
+            } else {
+                console.log(`${i}: Not anime or game`);
+                continue;
+            }
+        } catch (err) {
+            console.error(`${i}: ${err.message}`);
+            if (err.message.includes("E11000")) {
+                continue;
+            }
+            bangumiList.push(i);
+            continue;
+        }
+        console.log(`${i}: ${bangumiObject.name} | ${bangumiObject.name_cn || ''} has been inserted.`);
+    }
+}
+
+main().catch(console.dir).finally(() => {
+    client.close();
+});
